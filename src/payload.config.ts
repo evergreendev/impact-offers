a9b08e805bbaaaa0896a0516f53de9c64aa06132
Joe simpson <joe@egmrc.com>
2025-10-08 16:44:11 -0600

    feat: integrate Nodemailer for email functionality
    - Added Nodemailer configuration to handle email transport.
    - Integrated `nodemailerAdapter` for Payload CMS email plugin.
    - Updated dependencies to include Nodemailer.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,5 @@
 // storage-adapter-import-placeholder
+import * as nodeMailer from "nodemailer";
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
@@ -11,11 +12,26 @@ import { Media } from './collections/Media'
 import { Companies } from './collections/Companies'
 import { Offers } from './collections/Offers'
 import { Redemptions } from './collections/Redemptions'
+import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
+const transporter = nodeMailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT)||587,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
 export default buildConfig({
+  email: nodemailerAdapter({
+    defaultFromAddress: 'impact@mail.egmrc.com',
+    defaultFromName: 'Impact Offers',
+    transport: transporter
+  }),
   admin: {
     user: Users.slug,
     importMap: {
